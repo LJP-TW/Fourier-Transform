865207350cbcc6cfe074669aadc916b189d37aea
shrshiun <shrshiun84@gmail.com>
2019-06-12 17:01:34 +0800

    high&low pass

--- Fourier Transform/FT2.cpp
+++ Fourier Transform/FT2.cpp
@@ -2,12 +2,76 @@
 
 using namespace std;
 
-void FT::LowpassFilter(double** Real, double** Img, double** filter)
+void FT::LowpassFilter(int** InputImage, int** OutputImage, double ** FreqReal, double ** FreqImag, int h, int w)
 {
-    cout << "LowpassFilter" << endl;
+	int M = h;
+	int N = w;
+	double**pFreq = new double*[M];
+	for (int newcnt = 0;newcnt < M;newcnt++)
+	{
+		pFreq[newcnt] = new double[N];// 傅立葉頻率陣列
+	}
+	for (int forzero_i = 0;forzero_i < M;forzero_i++)//初始化
+	{
+		for (int forzero_j = 0;forzero_j < N;forzero_j++)
+		{
+			pFreq[forzero_i][forzero_j] = 0.0f;
+		}
+	}
+	//-------------------------------------------
+	double cutoff = w / 16;
+	double n = 4.0;
+	for (int i = 0;i < M;i++)
+	{
+		for (int j = 0;j < N;j++)
+		{
+			double Huv = 1.0 / (1.0 + pow(hypot<double, double>(j-h/2, i-w/2) / cutoff, 2.0*n));
+			//pFreq[i][j] *= Huv;
+			pFreq[i][j] = sqrt(pow(FreqReal[i][j], 2)*Huv + pow(FreqImag[i][j], 2)*Huv);
+			OutputImage[i][j] = pFreq[i][j];
+		}
+	}
+	for (int delcnt = 0;delcnt < M;delcnt++)
+	{
+		delete[] pFreq[delcnt];
+	}
+	delete[]pFreq;
+    //cout << "LowpassFilter" << endl;
 }
 
-void FT::HighpassFilter(double** Real, double** Img, double** filter)
+void FT::HighpassFilter(int** InputImage, int** OutputImage, double ** FreqReal, double ** FreqImag, int h, int w)
 {
+	int M = h;
+	int N = w;
+	double**pFreq = new double*[M];
+	for (int newcnt = 0;newcnt < M;newcnt++)
+	{
+		pFreq[newcnt] = new double[N];// 傅立葉頻率陣列
+	}
+	for (int forzero_i = 0;forzero_i < M;forzero_i++)//初始化
+	{
+		for (int forzero_j = 0;forzero_j < N;forzero_j++)
+		{
+			pFreq[forzero_i][forzero_j] = 0.0f;
+		}
+	}
+	//-------------------------------------------
+	double cutoff = w / 8;
+	double n = 4.0;
+	for (int i = 0;i < M;i++)
+	{
+		for (int j = 0;j < N;j++)
+		{
+			double Huv = 1.0-(1.0 / (1.0 + pow(hypot<double, double>(j-h/2, i-w/2) / cutoff, 2.0*n)));
+			//pFreq[i][j] *= Huv;
+			pFreq[i][j] = sqrt(pow(FreqReal[i][j], 2)*Huv + pow(FreqImag[i][j], 2)*Huv);
+			OutputImage[i][j] = pFreq[i][j];
+		}
+	}
+	for (int delcnt = 0;delcnt < M;delcnt++)
+	{
+		delete[] pFreq[delcnt];
+	}
+	delete[]pFreq;
     cout << "HighpassFilter" << endl;
 }
--- Fourier Transform/MyForm2.cpp
+++ Fourier Transform/MyForm2.cpp
@@ -5,8 +5,75 @@ using namespace FourierTransform;
 
 System::Void MyForm::lowpassFilterToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e)
 {
+	
+	int w = dataManager->GetImageWidth();
+	int h = dataManager->GetImageHeight();
+
+	// 利用傅立葉之平移性，平移頻率
+	/*for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			int valuePixeli = dataManager->GetInputImage()[i][j];
+			valuePixeli = valuePixeli * pow((float)-1, (float)(i + j));
+			dataManager->SetPixel(j, i, valuePixeli);
+		}
+	}*/
+
+	fourierTransformMethod->LowpassFilter(dataManager->GetInputImage(), dataManager->GetOutputImage(), dataManager->GetFreqReal(), dataManager->GetFreqImag(), h, w);
+	Bitmap^ LPFImage = gcnew Bitmap(w, h);
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			int valuePixeli = dataManager->GetOutputImage()[i][j];
+			if (valuePixeli > 255)
+			{
+				valuePixeli = 255;
+			}
+			else if (valuePixeli < 0)
+			{
+				valuePixeli = 0;
+			}
+			LPFImage->SetPixel(j, i, Color::FromArgb(valuePixeli, valuePixeli, valuePixeli));
+		}
+	}
+	pictureBox_OutputImage->Image =LPFImage;
 }
 
 System::Void MyForm::highpassFilterToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e)
 {
+	int w = dataManager->GetImageWidth();
+	int h = dataManager->GetImageHeight();
+
+	// 利用傅立葉之平移性，平移頻率
+	/*for (int i = 0; i < h; i++)
+	{
+	for (int j = 0; j < w; j++)
+	{
+	int valuePixeli = dataManager->GetInputImage()[i][j];
+	valuePixeli = valuePixeli * pow((float)-1, (float)(i + j));
+	dataManager->SetPixel(j, i, valuePixeli);
+	}
+	}*/
+
+	fourierTransformMethod->HighpassFilter(dataManager->GetInputImage(), dataManager->GetOutputImage(), dataManager->GetFreqReal(), dataManager->GetFreqImag(), h, w);
+	Bitmap^ HPFImage = gcnew Bitmap(w, h);
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			int valuePixeli = dataManager->GetOutputImage()[i][j];
+			if (valuePixeli > 255)
+			{
+				valuePixeli = 255;
+			}
+			else if (valuePixeli < 0)
+			{
+				valuePixeli = 0;
+			}
+			HPFImage->SetPixel(j, i, Color::FromArgb(valuePixeli, valuePixeli, valuePixeli));
+		}
+	}
+	pictureBox_OutputImage->Image = HPFImage;
 }
